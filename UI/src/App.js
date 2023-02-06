ef38e160eabaaf169fabc8007c272064035b258f
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-06 17:47:21 -0500

    initial commit

---
+++ UI/src/App.js
@@ -0,0 +1,414 @@
+function App() {
+  return (
+    <div className="App">
+      <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Raleway:wght@700&display=swap"
+        rel="stylesheet"
+      />
+
+      <nav id="navbar">
+        <header id="how">How to Dog</header>
+        <ul>
+          <li>
+            <a class="nav-link" href="#Introduction">
+              Introduction
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#What_you_should_already_know">
+              What you should already know
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Thunder_Storms_and_Vacuums">
+              Thunder Storms and Vacuums
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Hello_Squirrel">
+              Hello Squirrel
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Smells">
+              Smells
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Declaring_Smells">
+              Declaring Smells
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Smelling_Poop">
+              Smelling Poop
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Global_Smells">
+              Global Smells
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Snack_Types">
+              Snack Types
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Human_Statements">
+              Human Statements
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#if_cat..._statements">
+              If cat... Statements
+            </a>
+          </li>
+
+          <li>
+            <a class="nav-link" href="#Stranger_Declarations">
+              {' '}
+              Stranger Declarations
+            </a>
+          </li>
+          <li>
+            <a class="nav-link" href="#Reference">
+              Reference
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <main id="main-doc">
+        <section class="main-section" id="Introduction">
+          <header>Introduction</header>
+          <p>
+            Knowing how to dog is a skill and an art, and we dogs need to master the basic
+            requirements needed in order to dog succesfully. It's not an easy job, but some's got to
+            do it and no better creatures than us!
+          </p>
+
+          <p>
+            How to dog is hard but rewarding work and there is a lot invovled in being the best
+            friend humans ever had. This guide will go through some of that lost dog art and give
+            some basic instructions on how to start practising these skills.{' '}
+          </p>
+        </section>
+
+        <section class="main-section" id="What_you_should_already_know">
+          <header> What you should already know</header>
+          <p>This guide assumes you have the following basic background:</p>
+          <ul>
+            <li>
+              A general understanding of the best places to snooze and how to take over the human's
+              bed at night (SPRAWL tactics).
+            </li>
+            <li>Good working knowledge of the best begging techniques (SAD EYES strategy).</li>
+            <li>
+              Some cuddling experience. If you are new to cuddling, try one of the tutorials linked
+              on the main page about hugs.
+            </li>
+          </ul>
+        </section>
+
+        <section class="main-section" id="Thunder_Storms_and_Vacuums">
+          <header>Thunder Storms and Vacuums</header>
+          <p>
+            Now, we know both of these topics can be sensitve areas for most of us, but Doggo ipsum
+            ruff you are doing me the shock borkdrive adorable doggo lotsa pats big ol, sub woofer
+            pupperino extremely cuuuuuute lotsa pats.{' '}
+          </p>
+          <p>
+            We appreciate those of you who have overcome these fears and though you are few and far
+            between, we raise our water bowls to your bravery. Doggo ipsum ruff Stop it fren tungg
+            clouds borkdrive heckin, tungg blop shoober. Woofer pupper extremely cuuuuuute aqua
+            doggo pupper yapper thicc bork, wrinkler smol borking doggo with a long snoot for pats
+            noodle horse adorable doggo he made many woofs adorable doggo.{' '}
+          </p>
+          <p>
+            Together with the previously mentioned support groups, we can be there for our canines
+            in need and support each other through the loud noises of the world.
+          </p>
+        </section>
+
+        <section class="main-section" id="Hello_Squirrel">
+          <header>Hello Squirrel</header>
+          <p>
+            Try not to get too distracted thinking about those shiesty little critters while we talk
+            about this next topic, it's something we are all plagued with and we need to talk about
+            how to manage the loss of focus we fall victem to when those long furry tails cross our
+            path.{' '}
+          </p>
+          <p>
+            This is what most of you might experience when catching whiff of the sneaky sneaks:{' '}
+          </p>
+
+          <code>
+            {`function greetMe(squirrel) = `}
+            <br />
+            {`{alert('Hello ' + squirrel)}`}
+          </code>
+
+          <p>
+            While it's a great way to say hellow to the wee squirrel, it only leads to trouble and
+            often a yank on the chain if we get too involved. Doggo ipsum ruff squeak toy chihuahua
+            stay pug stay sit pretty play dead roll over catch, lap dog greyhound pit bull come. Dog
+            toy collar dog great dance squirrel kibbles chew toy pomsky dachshund. Dog bone take it
+            pug paw come, Morkie squirrel play dead german shephard english mastiff shake.
+          </p>
+
+          <p>
+            Please make note of these following other creatures that are variations of the
+            neighbourhood squirrel that you may come across. You should also try to not get lost in
+            their allure:
+          </p>
+          <div class="img">
+            <img
+              id="squirrel"
+              src="https://pics.me.me/the-official-field-guide-for-dogs-squirrel-sneaky-squirrel-sky-31853460.png"
+              alt="squirrel-types"
+            />
+          </div>
+          <p>In short, this is the addition we need to fight: </p>
+          <code>
+            squirrel-sighting: distracted; <br />
+            squirrel-focus: 100%;
+            <br />
+            chase-at: top-speed;
+          </code>
+        </section>
+
+        <section class="main-section" id="Smells">
+          <header>Smells</header>
+          <p>
+            This is where the majority of our focus will be for this documentation. Doggo ipsum
+            ruffLeash dog house bell lap dog rottweiler shih tzu greyhound. Shih tzu shih tzu milk
+            bone german shephard german shephard dog bone pit bull doberman pinscher poodle.{' '}
+          </p>
+          <p>
+            Doggo ipsum ruff Beagle husky rottweiler dachshund chihuahua lap dog, roll over fetch
+            dog house squirrel bite. Heel pomeranian boxer, k9 bell dachshund maltese chew toy
+            chihuahua greyhound pit bull dog sit pretty puppy.{' '}
+          </p>
+          <p>
+            {' '}
+            Doggo ipsum ruff Pit bull chew toy catch, roll over squeak toy jump chase tail english
+            mastiff chew toy pug beagle. German shephard pomeranian jump stay speak chihuahua poodle
+            english mastiff. Jump stand catch doberman pinscher jump chihuahua squeak toy, bark
+            leave it k9 take it bulldog.{' '}
+          </p>
+        </section>
+
+        <section class="main-section" id="Declaring_Smells">
+          <header>Declaring Smells</header>
+          <p>
+            Some of us are better or more obvious when we find a smell we like, you might see those
+            kids react as such:
+          </p>
+          <code>
+            sniff-sniff: definately-found-something; /* nose points steadfast in direction of smell
+            */
+            <br />
+          </code>
+          <p>For others the approach may be more sublte:</p>
+          <code>sniff-sniff: hmmm-that's-interesting; /* sniffs around some more */</code>
+          <p>Either approach is fine, what you want to avoid is:</p>
+          <code>
+            sniff-sniff: what-is-that; <br />
+            yank-force: 250%; /* hard yank on leash in direction of smell */
+          </code>
+        </section>
+
+        <section class="main-section" id="Smelling_Poop">
+          <header>Smelling Poop</header>
+          <p>
+            Some of the best smells are from poop, but the humans don't appreciate it like we do and
+            are often upset when we get to close to poop. Doggo ipsum ruff Lotsa pats heckin angery
+            woofer vvv heckin good boys and girls, corgo adorable doggo.{' '}
+          </p>
+          <p>
+            So Doggo ipsum ruff Puppies yorkshire terrier k9, paw shih tzu squeak toy rottweiler
+            bring it poodle tennis ball leap down doberman pinscher sit pretty. Pit bull poodle
+            growl st bernard dog toy, jump bite pomeranian play dead shake leash leash doberman
+            pinscher. Having said all that, here is the best approach to poop sniffing:
+          </p>
+          <code>
+            sniff-setting: scan;
+            <br />
+            scan: 2wce; /* double check the human is not looking */
+            <br />
+            advancing-strength: medium; /* only continue if human not looking */
+          </code>
+          <p>
+            This behavior changes, when using the treat declaration that was introduced in
+            TREATScript 2015:
+          </p>
+          <code>
+            {`if (true) { `} <br />
+            {`let y = poop; `} <br />
+            {`console.log(poop);`} <br />
+            {` }`} <br />
+            {` // ReferenceError: poop is not defined`}`} <br />
+          </code>
+        </section>
+
+        <section class="main-section" id="Global_Smells">
+          <header>Global Smells</header>
+          <p>
+            There are some smells that you will always be able to smell, no matter if you are
+            outside, inside or at the neighbour human's house, and that smell is SNACKS!
+          </p>
+          <p>
+            Doggo ipsum ruff Vet jump bark tennis ball take it tug boxer tail squirrel. Tennis ball
+            leave it chow chow, growl yorkshire terrier puppy dog bone beagle chase tail dachshund
+            rottweiler.{' '}
+          </p>
+        </section>
+
+        <section class="main-section" id="Snack_Types">
+          <header>Snack Types</header>
+          <p>The latest D-O-G University reports define seven snack types:</p>
+          <ul>
+            <li>Primitive snack types: </li>
+            <ul>
+              <li>PizzaBones - A top-level property whose value is defined as 'crust'.</li>
+              <li>Cookies - goodness, stored in small square boxes, on your human's shelf.</li>
+              <li>
+                Cheese - A logical data type that can have only the values 'stinky' or 'creamy'
+              </li>
+              <li>
+                Carrots - new in TREATScript2015 - A data type whose instances are unique and
+                immutable.
+              </li>
+            </ul>
+            <br />
+            <li>Non-primitive snack types:</li>
+            <ul>
+              <li>NutBut - It represents a peanut butter in TREATScript.</li>
+              <li>Meat - Represents a collection of similar edible meaty, elements. </li>
+              <li>Crackers - A special type denoting a cardboard value. </li>
+            </ul>
+          </ul>
+        </section>
+        <section class="main-section" id="Human_Statements">
+          <header>Human Statements</header>
+          <p>
+            Huamns like to ask us to do things before they give us treats, while it's not neccisary
+            (we'll take treats without needing the words) we should remember how cute it is that
+            they do this and humour them. Here are the words you might here and what they meant for
+            you to do:
+          </p>
+          <code>
+            {`const stay = "don't chase anything"`} <br />
+            {`let sit = "put your butt down and don't chase anythin"`} <br />
+            {`const paw = "touch my hand"`} <br />
+            {`const heel = "walk bside me and don't chase anything"`} <br />
+            {`const down = "put your belly on the floor and dont chase aything"`} <br />
+            {`var fetch = "chase this thing - followed by them throwing something away from you"`}{' '}
+            <br />
+          </code>
+
+          {/* <ul>
+            <li>stay: don't chase anything</li>
+            <li>sit: "put your butt down and don't chase anythin"g</li>
+            <li>paw: touch my hand</li>
+            <li>heel: walk bside me and don't chase anything</li>
+            <li>down: put your belly on the floor and dont chase aything</li>
+            <li>fetch: chase this thing - followed by them throwing something away from you</li>
+          </ul> */}
+        </section>
+        <section class="main-section" id="if_cat..._statements">
+          <header>If cat... Statements</header>
+          <p>
+            Cats. There is nothing that clouds our judgement more then a smug cat. There is only one
+            way to know if it's safe to act on those urges and we have documented those instructions
+            here:
+          </p>
+          <p class="rule">
+            Use the if cat statement to execute an action if the logical condition is true. Use
+            restraint to not execute the cat if the condition is false. If cat statements break down
+            as follows:
+          </p>
+          <code>
+            {`if (condition) {`}
+            <br />
+            {`statement_1;`} <br />
+            {` } else { `} <br />
+            {`statement_2;`}
+            <br />
+            {` }`}
+          </code>
+
+          <p>
+            You may also compound the statements using else if to have multiple conditions tested in
+            sequence, as follows:
+          </p>
+          <code>
+            {`if (condition_1) { `} <br />
+            {`statement_1; `} <br />
+            {`} else if (condition_2) { `} <br />
+            {`statement_2;`} <br />
+            {`} else if (condition_n) { `} <br />
+            {`statement_n; `} <br />
+            {`} else { statement_last; }`}
+          </code>
+
+          <p>Here are some examples of these statements.</p>
+          <code>
+            {`if (cat is sleeping) {stay;}`}
+            <br />
+            <br />
+            {`if (cat is staring_at_you) {`} <br />
+            {`chase;`}
+            <br />
+            {`} else {`} <br />
+            {`stay;`} <br />
+            {`}`} <br />
+            <br />
+            {`if (cat is spraying_in_your_backyard) {chase;} `}
+            <br />
+            <br />
+            {`if (cat is cleaning_it's_wiskars) {chase}`} <br />
+            <br />
+            {`if (cat is licking_it's_butt) {`} <br />
+            {`stay;`} <br />
+            {`} else if (cat is just_stretching) {`} <br />
+            {`chase;`} <br />
+            {`}`}
+          </code>
+        </section>
+
+        <section class="main-section" id="Stranger_Declarations">
+          <header>Stranger Declarations</header>
+          <p>
+            It's very important to let your people know when there is someone coming to your home
+            that does not belong there. They have very bad judgement and NEED our help with it comes
+            to deciphering which strangers are good and which are bad. Follow these insructions to
+            let your people know who is trustworth:
+          </p>
+          <code>
+            {`if (human.smells_good) {`} <br />
+            {`wag_tail;`} <br />
+            {`} else if (human.smells_bad) {`} <br />
+            {`bark_loud;}`}
+          </code>
+        </section>
+
+        <section class="main-section" id="Reference">
+          <header>Reference</header>
+          <p>
+            For more help on these and other topics check out{' '}
+            <a href="#" target="_blank">
+              How to Dog.
+            </a>
+          </p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
